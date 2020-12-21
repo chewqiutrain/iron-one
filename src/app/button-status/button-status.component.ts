d56646a416549b8091bc2a4ee582125c6488d5b0
Chew Lee <leeyingqiu@gmail.com>
2020-12-21 23:18:39 +0800

    thanks buschman

--- src/app/button-status/button-status.component.ts
+++ src/app/button-status/button-status.component.ts
@@ -6,12 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-status.component.css']
 })
 export class ButtonStatusComponent implements OnInit {
+  
+  response: String = "";
 
   Messages: String[] = [
     "yellow apple",
     "fat man",
     "orderly buttons"
   ];
+
+  
 
 
   constructor() { }
@@ -23,6 +27,7 @@ export class ButtonStatusComponent implements OnInit {
     const randomIndex = Math.floor(Math.random() * 3);
     const randomMessage = this.Messages[randomIndex];
     console.log(`buttonPressed | randomIndex: ${randomIndex} | randomMessage: ${randomMessage}`);
+    this.response = randomMessage;
     return randomMessage;
   }
 
